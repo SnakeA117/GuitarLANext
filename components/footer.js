0db783d2f9b21ddf1ef5bb4f8dbd7b0e626a20c3
SnakeA117 <ltdrake1984@gmail.com>
2023-08-18 15:52:42 -0500

    Added CSS for Footer

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,32 @@
+import Link from "next/link"
+import styles from "../styles/footer.module.css"
 
 export default function Footer() {
   return (
-    <div>footer</div>
+    <footer className={styles.footer}>
+        <div className={`contenedor ${styles.contenido}`}>
+        <nav className={styles.navegacion}>
+                <Link legacyBehavior href="/">
+                  Home
+                </Link>
+
+                <Link legacyBehavior href="/us">
+                  About Us
+                </Link>
+
+                <Link legacyBehavior href="/blog">
+                  Blog
+                </Link>
+
+                <Link legacyBehavior href="/store">
+                  Store
+                </Link>
+          
+            </nav>
+            <p className={styles.copyright}>
+              All rights reserved { new Date().getFullYear()}
+            </p>
+        </div>
+    </footer>
   )
 }
--- components/header.js
+++ components/header.js
@@ -1,28 +1,44 @@
 import Image from "next/image"
 import Link from "next/link"
 import styles from "../styles/header.module.css"
+import { useRouter } from "next/router"
 
 export default function Header() {
+
+    const router = useRouter()
   return (
     <header className={styles.header}>
      <div className={`contenedor ${styles.barra}`}>
-            <Image src="/img/logo.svg" width={300} height={40} alt="logo image"/>
+
+                <Link legacyBehavior href="/">
+                    <a>
+                    <Image src="/img/logo.svg" width={300} height={40} alt="logo image"/>
+                    </a>
+                </Link>
 
             <nav className={styles.navegacion}>
-                <Link href="/">
+                <Link legacyBehavior href="/">
+                    <a className={router.pathname === '/' ? styles.active : ''}>
                     Home
+                    </a>
                 </Link>
 
-                <Link href="/us">
-                    About Us
+                <Link legacyBehavior href="/us">
+                <a className={router.pathname === '/us' ? styles.active : ''}>
+                    About us
+                    </a>
                 </Link>
 
-                <Link href="/blog">
+                <Link legacyBehavior href="/blog">
+                <a className={router.pathname === '/blog' ? styles.active : ''}>
                     Blog
+                    </a>    
                 </Link>
 
-                <Link href="/store">
+                <Link legacyBehavior href="/store">
+                <a className={router.pathname === '/store' ? styles.active : ''}>
                     Store
+                    </a>
                 </Link>
           
             </nav>
