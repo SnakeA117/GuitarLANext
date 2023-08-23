ee4157746134632fbb895500ee662b5bb80aa2ae
SnakeA117 <ltdrake1984@gmail.com>
2023-08-23 17:27:35 -0500

    Added home page components

--- pages/guitars/[url].js
+++ pages/guitars/[url].js
@@ -36,7 +36,6 @@ export async function getStaticPaths(){
         }
     }))
 
-    console.log(paths)
     return {
         paths,
         fallback: false
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,61 @@
 import Layout from "@/components/layout"
-import Link from "next/link"
+import styles from '../styles/grid.module.css'
+import Guitar from "@/components/guitar"
+import Post from "@/components/post"
 
-export default function Home() {
+export default function Home({guitars, posts}) {
+
   return (
     <>
     <Layout
       title={'Home'}
       description={'Music Blog, guitar sale and more'}
     >
-      <h1>Hello world in Next</h1>
+      <main className="contendor">
+          <h1 className="heading">Our collection</h1>
+          <div className={styles.grid}> 
+          {guitars.map(guitar => (
+            <Guitar 
+              key={guitar.id}
+              guitar={guitar.attributes}
+            />
+          ))}
+          </div>
+      </main>
+      <section className="contenedor">
+              <h2 className="heading">Blog</h2>
+              <div className={styles.grid}>
+              {posts?.map(post => (
+                  <Post 
+                    key={post.id}
+                    post={post.attributes}
+                  />
+              ))}
+            </div>
+      </section>
     </Layout>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const urlGuitars = `${process.env.API_URL}/guitars?populate=image`
+  const urlPosts = `${process.env.API_URL}/posts?populate=Image`
+
+  const [ resGuitars, resPosts ] = await Promise.all([
+    fetch(urlGuitars),
+    fetch(urlPosts)
+  ])
+
+  const [{ data: guitars}, {data: posts}] = await Promise.all([
+    resGuitars.json(),
+    resPosts.json()
+  ])
+
+  return {
+    props: {
+        guitars,
+        posts
+    }
+  }
 }
